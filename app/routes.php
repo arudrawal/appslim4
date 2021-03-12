3d6189faba18207e1e53d768e6d31363f534fd8f
arudrawal <arudrawal@gmail.com>
2021-03-11 16:52:15 -0800

    add twig view

--- app/routes.php
+++ app/routes.php
@@ -5,5 +5,5 @@ require_once __DIR__ . "/../vendor/autoload.php";
 use Slim\Psr7\Request as Request;
 use Slim\Psr7\Response as Response;
 
-$app->get('/appslim4/public/hello', \App\Controllers\HomeController::class . ':hello');
-$app->get('/appslim4/public/bye', \App\Controllers\HomeController::class . ':bye');
+$app->get('/appslim4/public/hello', \App\Controllers\HomeController::class . ':hello')->setName('hello');
+$app->get('/appslim4/public/bye', \App\Controllers\HomeController::class . ':bye')->setName('bye');
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,16 +6,23 @@ require_once __DIR__ . "/../vendor/autoload.php";
 
 //use Psr\Http\Message\ResponseInterface as Response;
 //use Psr\Http\Message\RequestInterrface as Request;
+use DI\Container;
 use Slim\Psr7\Request as Request;
 use Slim\Psr7\Response as Response;
 use Slim\Factory\AppFactory;
-use DI\Container;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 $container = new Container();
 
 AppFactory::setContainer($container);
 
+$container->set('view', function(){
+	return Twig::create(__DIR__.'/../views/', ['cache'=> '']);
+});
 $app = AppFactory::create();
+$app->add(TwigMiddleware::createFromContainer($app));
+
 $app->addRoutingMiddleware();
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
